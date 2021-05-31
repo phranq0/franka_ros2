f9a8c30fe27a4d06d45348ddc8f80c37d04fd752
wngfra <wngfra@gmail.com>
2021-05-31 18:36:37 +0200

    updated Dockerfile

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run -it --tty --device /dev/dri --user ubuntu --name $1 -v $(realpath ~)/ros2_ws:/ubuntu/ros2_ws -v /tmp/.X11-unix:/tmp/.X11-unix:rw --net=host -e DISPLAY=$DISPLAY -e XAUTHORITY wngfra/franka_ros2:latest
+docker run -it --tty --device /dev/dri --user ubuntu --name $1 -v $(realpath ~)/franka_ws:/ubuntu/franka_ws -v /tmp/.X11-unix:/tmp/.X11-unix:rw --net=host -e DISPLAY=$DISPLAY -e XAUTHORITY wngfra/franka_ros2:base
